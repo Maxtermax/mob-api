29018d94c1f6c1615447cafe367e0ac90d280f07
maxtermax <esnene02@gmail.com>
2020-04-05 15:37:09 -0500

    test auto deploy

--- core/app/index.js
+++ core/app/index.js
@@ -2,16 +2,25 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
+const { authMiddleware } = require("../app/authConfig");
 const bodyParser = require("body-parser");
 const Comments = require("./controllers/Comments");
 
-app.use(cors({ origin: ["http://localhost:3000"] }));
+app.use(
+  cors({ origin: ["http://localhost:3000", "https://82a091ba.ngrok.io"] })
+);
+app.use(authMiddleware);
 
-app.use(cors({ origin: true }));
 app.use(bodyParser.json());
 app.use(morgan("combined"));
 
-app.get("/comments/find/:id", Comments.findByMovieId);
-app.post("/comments/create", Comments.createRecord);
+app.get("/test", (req, res) => {
+  res.send({
+    msg: "Your Access Token was successfully validated!",
+  });
+});
+
+app.get("/api/v1/comments/find/:id", Comments.findByMovieId);
+app.post("/api/v1/comments/create", Comments.createRecord);
 
 module.exports = app;
---
+++ core/app/middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+const authConfig = {
+  domain: process.env.DOMAIN,
+  audience: process.env.IDENTIFIER,
+};
+module.exports = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`,
+  }),
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ["RS256"],
+});
