9ec577b87834415984a22874b3d824944295484f
maxtermax <esnene02@gmail.com>
2020-04-06 01:24:20 -0500

    include date created in response

--- core/database/models/Comment/createRecord.js
+++ core/database/models/Comment/createRecord.js
@@ -27,11 +27,11 @@ async function createRecord(payload) {
   const { ok, error, result } = await resolvePromise(
     Comment.create({ text, userId, movieId })
   );
-  logger({ text, userId, movieId });
-  logger({ ok, error, result });
+  // logger({ text, userId, movieId });
+  // logger({ ok, error, result });
   if (ok) {
     const { id = "" } = result;
-    return Promise.resolve({ ok: true, id });
+    return Promise.resolve({ ok: true, ...result });
   }
   return Promise.reject({ ...Exceptions.SERVER_ERROR, details: error });
 }
