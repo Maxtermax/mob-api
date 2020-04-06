46e45940f4551f80661920a1bc31341c05cb63a7
maxtermax <esnene02@gmail.com>
2020-04-06 03:22:19 -0500

    set fk

--- core/database/models/index.js
+++ core/database/models/index.js
@@ -2,7 +2,7 @@ module.exports = () => {
   const User = require("./User");
   const Comment = require("./Comment");
   User.hasMany(Comment);
-  Comment.belongsTo(User);
+  Comment.belongsTo(User, { foreignKey: "userId" });
 
   return {
     User,
